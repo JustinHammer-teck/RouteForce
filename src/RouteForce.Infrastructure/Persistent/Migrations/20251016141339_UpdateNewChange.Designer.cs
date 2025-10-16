d0d469e3e95fe1a48eeb0efa8a541a8ab417bfb5
Moritz Zimmermann <81556222+JustinHammer-teck@users.noreply.github.com>
2025-10-17 01:16:05 +1100

    [Chore] Update DB Migration

--- src/RouteForce.Infrastructure/Persistent/Migrations/20251016141339_UpdateNewChange.Designer.cs
+++ src/RouteForce.Infrastructure/Persistent/Migrations/20251016141339_UpdateNewChange.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RouteForce.Infrastructure.Persistent;
 
@@ -10,9 +11,11 @@ using RouteForce.Infrastructure.Persistent;
 namespace RouteForce.Infrastructure.Persistent.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251016141339_UpdateNewChange")]
+    partial class UpdateNewChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
